cb32495eef0c44d97dd11501534610b72163bf03
Hugo Luna <hugo_1199@hotmail.com>
2021-07-24 02:58:44 -0500

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Hugo Luna on 24/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Hugo Luna on 24/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
